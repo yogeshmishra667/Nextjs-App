0f4dd561d9a30b900b3d7c5dce9c6a97cd4685ee
yogesh mishra <yogeshmishra667@gmail.com>
2023-01-17 14:29:47 +0530

    routing at the client side

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import Link from 'next/Link';
 const home = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <p>This is the home page</p>
+      <Link href='/blog'>
+        Blog
+      </Link>
+
+      <Link href='/product'>
+       Product
+      </Link>
     </div>
   );
 }
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,12 @@
-const Product = () => {
+import Link from "next/link";
+
+const Product = ({productId = 34}) => {
   return (
     <div>
-      <h1>The Product Page</h1>
-      <h1>The Product Page 2 </h1>
-      <h1>The Product Page 3</h1>
+    <Link href="/product/1"><h1>The Product Page</h1></Link>
+    <Link href="/product/2"><h1>The Product Page 2 </h1></Link>
+    <Link href="/product/3"><h1>The Product Page 3</h1></Link>
+    <Link href={`/product/${productId}`}><h1>The Product Page {productId}</h1></Link>
     </div>
   );
 }
