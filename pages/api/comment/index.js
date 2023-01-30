535e29137874575e6086da01459ab347ff244d04
yogesh mishra <yogeshmishra667@gmail.com>
2023-01-30 19:33:22 +0530

    post request using next api endpoint

--- pages/api/comment/index.js
+++ pages/api/comment/index.js
@@ -1,4 +1,21 @@
 import { comments } from '../../../data/comment';
 export default function Comment(req, res) {
-  res.status(200).json(comments);
+  if (req.method === 'GET') {
+    res.status(200).json(comments);
+  }
+
+  if (req.method === 'POST') {
+    const { name, email, comment } = req.body;
+    if (!comment) {
+      res.status(422).json({ message: 'Invalid input.' });
+    }
+    const newComment = {
+      id: new Date().toISOString(),
+      name,
+      email,
+      comment,
+    };
+    comments.push(newComment);
+    res.status(201).json({ message: 'Added comment.', comment: newComment });
+  }
 }
--- pages/comment/index.js
+++ pages/comment/index.js
@@ -2,15 +2,38 @@ import { useState } from 'react';
 
 const Comment = () => {
   const [comments, setComments] = useState([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [comment, setComment] = useState('');
+
   const showCommentOnClick = async () => {
     const response = await fetch('http://localhost:8080/api/comment');
     const data = await response.json();
     setComments(data);
   };
 
+  const submitCommentOnClick = async () => {
+    const response = await fetch('http://localhost:8080/api/comment', {
+      method: 'POST',
+      body: JSON.stringify({ comment, name, email }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div>
       <h1>Comment</h1>
+      <label htmlFor="comment">Comment</label>
+      <input type="text" value={comment} onChange={(e) => setComment(e.target.value)} />
+      <label htmlFor="name">Name</label>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <label htmlFor="email">Email</label>
+      <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <button onClick={submitCommentOnClick}>Submit Comment</button>
       <button onClick={showCommentOnClick}>Show Comment</button>
       <ul>
         {comments.map((comment) => (
